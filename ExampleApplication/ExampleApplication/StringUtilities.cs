341778d4be63ad9bf6674195bdadfdb9165981a6
choo98 <48203082+choo98@users.noreply.github.com>
2019-05-16 15:15:22 +0800

    Add files via upload

---
+++ ExampleApplication/ExampleApplication/StringUtilities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExampleApplication
+{
+    public class StringUtilities
+    {
+        public string combineArrayStringWithDelimiter(string[] stringArray, string delimiter)
+        {
+            string str = default(string);
+            foreach (string item in stringArray)
+            {
+                str += item + delimiter;
+          
+           
+            }
+            return str.TrimEnd();
+        }
+        
+        public string replaceSpacesInStringByHyphens(string aString)
+        {
+            string[] stringArray = aString.Split(null);
+            return combineArrayStringWithDelimiter(stringArray, "-").TrimEnd('-');
+        } 
+    }
+
+}
